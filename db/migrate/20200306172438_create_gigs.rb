0e5bb7fad4bb52b194e41a357ecddf6550dd81e1
Riley Slayden <slaydenriley@gmail.com>
2020-03-06 11:24:53 -0600

    Generated Gig resource

---
+++ db/migrate/20200306172438_create_gigs.rb
@@ -0,0 +1,13 @@
+class CreateGigs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :gigs do |t|
+      t.string :name
+      t.date :date
+      t.time :time
+      t.integer :band_id
+      t.integer :venue_id
+
+      t.timestamps
+    end
+  end
+end

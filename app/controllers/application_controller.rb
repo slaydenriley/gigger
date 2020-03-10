753fc2dfe8f36dc4d726ecefc97f87ff738aaae3
Riley Slayden <slaydenriley@gmail.com>
2020-03-10 17:52:40 -0500

    Removed pundit and added cancancan gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
   helper_method :logged_in?
-  include Pundit
 
   def current_user
     User.find_by(id: session[:user_id]) if session[:user_id].present?

6199962f6de58fca963726695957e26c36ae8221
Riley Slayden <slaydenriley@gmail.com>
2020-03-11 16:13:25 -0500

    Configuring OAuth with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,15 +14,15 @@ class User < ApplicationRecord
 
   def self.create_with_omniauth(auth)
 
-    user = find_or_create_by(uid: auth['uid'], provider: auth['provider'])
-    user.email = auth[:info][:email]
-    user.password = auth['uid']
-    user.name = auth['info']['name']
-    if User.exists?(user.id)
-      user
+    @user = find_or_create_by(uid: auth['uid'], provider: auth['provider'])
+    @user.email = auth[:info][:email]
+    @user.password = SecureRandom.hex
+    @user.name = auth['info']['name']
+    @user.image = auth['info']['image']
+    if User.exists?(@user.id)
+      @user
     else
-      user.save!
-      user
+      @user.save
     end
   end
 end

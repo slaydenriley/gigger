d7978dcf68dcbe76992cc1139d0d489e0f360f5d
Riley Slayden <slaydenriley@gmail.com>
2021-02-26 14:29:53 -0700

    Oauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-OmniAuth.config.allowed_request_methods = [:post, :get]
+# OmniAuth.config.allowed_request_methods = [:post, :get]
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
